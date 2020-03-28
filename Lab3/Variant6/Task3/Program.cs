2ada34cab5749c02460e3a1c8ac994c05ee0ae44
Matvey <matvey2k@gmail.com>
2020-03-28 20:39:41 +0300

    Добавлено 3 задание 6 варианта

---
+++ Lab3/Variant6/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int amount = 0;
+            if (str.Count(i => i == ':') == 2)
+            {
+                amount = str.Substring(str.IndexOf(':'), str.LastIndexOf(':') - str.IndexOf(':') - 1).Length;
+                Console.WriteLine($"Количетсво символов между двоеточиями: {amount}");
+            }
+            else
+            {
+                Console.WriteLine("Строка не удовлетворяет условиям");
+            }
+        }
+    }
+}
